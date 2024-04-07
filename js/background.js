b7e04d20faaa99bc1e14f6e869a24ac829b75114
subin1028 <2lsbapple@gmail.com>
2024-04-07 12:17:36 +0900

    add blank

--- js/background.js
+++ js/background.js
@@ -1,6 +1,7 @@
 const images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
+
 const bigImage = document.createElement("img");
 bigImage.src = `img/${chosenImage}`;
 bigImage.classList.add("background");
